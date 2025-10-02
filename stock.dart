a1699d584604ffebaee8d0e0002086803dcb9637
rahul <meetmeerahul@gmail.com>
2025-10-02 10:38:36 +0530

    Max Prfoit stock

--- stock.dart
+++ stock.dart
@@ -1,32 +1,17 @@
 void main() {
-  List<int> arr = [8, 6, 6, 9, 5, 9, 2, 4, 6, 4];
+  List<int> arr = [7, 1, 5, 3, 6, 4];
 
-  Map<int, int> count = {};
+  int minPrice = 100000000000;
+  int profit = 0;
 
   for (int i = 0; i < arr.length; i++) {
-    count[arr[i]] = (count[arr[i]] ?? 0) + 1;
-  }
+    if (arr[i] < minPrice) {
+      minPrice = arr[i];
+    }
 
-  List<int> key = [];
-  List<int> value = [];
-  for (var entry in count.entries) {
-    key.add(entry.key);
-    value.add(entry.value);
-  }
-
-  for (int i = 0; i < value.length; i++) {
-    for (int j = i + 1; j < value.length; j++) {
-      if (value[i] < value[j]) {
-        int tempVal = value[i];
-        value[i] = value[j];
-        value[j] = tempVal;
-
-        int temp = key[i];
-        key[i] = key[j];
-        key[j] = temp;
-      }
+    if (arr[i] - minPrice > profit) {
+      profit = arr[i] - minPrice;
     }
   }
-
-  print(key);
+  print(profit);
 }
