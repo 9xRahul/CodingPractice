a14e619ab8dbb8a29c7c799f0a7c7002108557e7
rahul <meetmeerahul@gmail.com>
2025-10-29 09:41:04 +0530

    reverse an integer

---
+++ ReverseAnInteger.dart
@@ -0,0 +1,14 @@
+void main() {
+  int n = 123;
+
+  int temp = n;
+
+  int? reverse = 0;
+
+  while (temp > 0) {
+    int digit = temp % 10;
+    reverse = reverse! * 10 + digit;
+    temp = temp ~/ 10;
+  }
+  print(reverse);
+}
